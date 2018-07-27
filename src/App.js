57ee6da816744cbedf977b8e2ade8d81d55b5ae5
Glitch (cuadra) <none>
2018-07-27 05:27:35 +0000

    👨‍❤️‍💋‍👨🍃 Header and Description

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './components/header/header';
+import ContentBox from './components/content-box/content-box';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="page">
+        <Header/>
+        <div id="main-container">
+          <ContentBox />
+        </div>
       </div>
     );
   }
---
+++ src/components/header/header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div id="logo">
+          <img src="https://image.ibb.co/dtMSyx/Personal_Logo_02.png"/>
+        </div>
+        <h1>
+          . . .
+        </h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
